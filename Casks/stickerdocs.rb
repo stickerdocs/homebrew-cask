38ec5066d8d285aa1c86068d2c0e0d91326e40a7
Anthony Fielding <anthony@stickerdocs.com>
2024-01-30 19:34:10 +0000

    Create stickerdocs.rb

---
+++ Casks/stickerdocs.rb
@@ -0,0 +1,12 @@
+cask "stickerdocs" do
+    version "1.0.0.16"
+    sha256 "99395cdb0c4760a1333ebe201c280b4550831307349db94ea405adb826a4e0f1"
+  
+    url "https://stickerdocs.com/files/StickerDocs-macOS-#{version}.zip"
+    name "StickerDocs"
+    desc "Secure and affordable file and photo sharing for families"
+    homepage "https://stickerdocs.com"
+  
+    app "StickerDocs.app"
+  end
+  
